5d54b60e5b417979131c0a226abfd9a48d891372
Jake Rice <jrice189@gmail.com>
2022-11-22 10:34:17 -0800

    Solve Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...removals) {
+  for (item of removals) {
+    const index = arr.indexOf(item);
+    if (index>=0) arr.splice(index,1);
+  }
+  return arr;
 };
 
 // Do not edit below this line
