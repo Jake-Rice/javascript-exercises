cecca8b5bd15eb64da20fe43762184c11cf3b0e9
Jake Rice <jrice189@gmail.com>
2022-11-22 10:20:12 -0800

    Solve Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numRepeats) {
+  if (numRepeats<0) return "ERROR";
+  let stringToOutput = "";
+  for (let count=0; count<numRepeats; count++) {
+    stringToOutput+=stringToRepeat;
+  }
+  return stringToOutput;
 };
 
 // Do not edit below this line
