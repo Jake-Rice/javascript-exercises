98ace8d8deb913c010da146f420026e34c3ac567
Jake Rice <jrice189@gmail.com>
2022-11-22 10:44:21 -0800

    Solve Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(startIndex, endIndex) {
+  if (typeof startIndex !== 'number' ||
+      typeof endIndex !== 'number' ||
+      startIndex < 0 ||
+      endIndex < 0) {
+    return "ERROR"
+  }
+  let start, end;
+  if (startIndex > endIndex) {
+    end = startIndex;
+    start = endIndex;
+  }
+  else {
+    start = startIndex;
+    end = endIndex;
+  }
+  let sum = 0;
+  for (let i=start; i<=end; i++) {
+    sum+=i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
