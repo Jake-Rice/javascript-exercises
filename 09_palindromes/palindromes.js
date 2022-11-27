e11199c463ca14391da18ae51e9c0199d9111c1a
Jake Rice <jrice189@gmail.com>
2022-11-27 10:21:27 -0800

    Solve Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const arr = str.toLowerCase()
+                 .split('')
+                 .filter(e => /[a-z]/.test(e));
+  if (arr.length%2 !== 0) arr.splice((arr.length-1)/2,1); //remove middle letter
+  const sub1 = arr.slice(0,arr.length/2).join('');
+  const sub2 = arr.slice(arr.length/2).reverse().join('');
+  return sub1===sub2;
 };
 
 // Do not edit below this line
