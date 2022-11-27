6d3d17be5ab16bfd58e5a81e9f469539878f95e9
Jake Rice <jrice189@gmail.com>
2022-11-27 10:47:29 -0800

    Solve Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  return arr.reduce((oldest, current) => {
+    if (!current.yearOfDeath) current.yearOfDeath = (new Date()).getFullYear();
+    const currentAge = current.yearOfDeath - current.yearOfBirth;
+    const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    return (currentAge>oldestAge) ? current : oldest;
+  },arr[0]);
 };
 
 // Do not edit below this line
