5063a2fe9922467cb7641e965aff64da834d6876
Jake Rice <jrice189@gmail.com>
2022-11-27 10:31:54 -0800

    Solve Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+  if (typeof input === "string") input = +input;
+  if (input<0) return "OOPS";
+  let previous = 1;
+  let current = 1;
+  let index = 2;
+  while (index<input) {
+    let ph = current;
+    current = previous+current;
+    previous = ph;
+    index++;
+  }
+  return current;
 };
 
 // Do not edit below this line
